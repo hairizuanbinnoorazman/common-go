d70754bcb9ae4d822dd240a9feaa655c85e689f4
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-09-19 01:11:17 +0800

    fix: Added social user login functionality

--- models/socialUser/socialUser.go
+++ models/socialUser/socialUser.go
@@ -1,6 +1,13 @@
 package socialUser
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/oauth2"
+)
+
+var ErrTokenUnavailable = errors.New("Unable to generate token from user.")
 
 type User struct {
 	ID           string    `json:"id"`
@@ -14,4 +21,20 @@ type User struct {
 	LastLoginAt  time.Time `json:"-"`
 	CreatedAt    time.Time `json:"-"`
 	UpdatedAt    time.Time `json:"-"`
+}
+
+func (u *User) Validate() error {
+	return nil
+}
+
+func (u User) GetToken() (*oauth2.Token, error) {
+	if u.RefreshToken == "" || u.AccessToken == "" || u.TokenType == "" {
+		return nil, ErrTokenUnavailable
+	}
+	token := oauth2.Token{}
+	token.RefreshToken = u.RefreshToken
+	token.AccessToken = u.AccessToken
+	token.Expiry = u.Expiry
+	token.TokenType = u.TokenType
+	return &token, nil
 }
