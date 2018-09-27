238bfb0f1bbd7d87ff528575fb845fd08a393b46
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-09-27 08:28:24 +0800

    feat: Added slack chat for sample use

--- chat/slack.go
+++ chat/slack.go
@@ -1,10 +1,9 @@
 package chat
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"net/http"
+	"net/url"
 )
 
 type Slack struct {
@@ -13,21 +12,15 @@ type Slack struct {
 }
 
 func (s Slack) SendMessage(ctx context.Context, client *http.Client, message string) error {
-	url := "https://slack.com/api/chat.postMessage"
-	type postMessageBody struct {
-		Token   string `json:"token"`
-		Channel string `json:"channel"`
-		Text    string `json:"Text"`
-	}
-	body := postMessageBody{Token: s.SlackToken, Channel: s.ChannelID, Text: message}
-	bodyJSON, err := json.Marshal(body)
+	slackUrl := "https://slack.com/api/chat.postMessage"
+	resp, err := client.PostForm(slackUrl, url.Values{
+		"token":   []string{s.SlackToken},
+		"channel": []string{s.ChannelID},
+		"text":    []string{message},
+	})
 	if err != nil {
 		return err
 	}
-	reader := bytes.NewReader(bodyJSON)
-	_, err = client.Post(url, "application/json", reader)
-	if err != nil {
-		return err
-	}
+	defer resp.Body.Close()
 	return nil
 }
